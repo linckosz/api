e042bf98d0b966a65e40069b238f92c67f4e7f2d
Bruno Martin <bruno.martin@lincko.com>
2016-05-17 16:45:11 +0800

    API 20160517 02 Bruno - Add file PivotUsers.php

---
+++ bundles/lincko/api/models/libs/PivotUsers.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace bundles\lincko\api\models\libs;
+
+use \bundles\lincko\api\models\libs\ModelLincko;
+use \bundles\lincko\api\models\data\Users;
+
+class PivotUsers extends ModelLincko {
+
+	protected $connection = 'data';
+
+	protected $table = 'users_x_';
+	protected $morphClass = 'users_x_';
+
+	protected $dates = array();
+
+	protected static $permission_sheet = array(
+		0, //[R] owner
+		0, //[R] max allow || super
+	);
+	
+////////////////////////////////////////////
+
+	//Many(comments) to One(Users)
+	public function users(){
+		return $this->belongsTo('\\bundles\\lincko\\api\\models\\data\\Users', 'users_id');
+	}
+
+	public function __construct(array $attributes = array()){
+		$model = new Users;
+		foreach ($attributes as $key => $value) {
+			if($key=='table'){
+				if($model->tableExists($this->table.$value)){
+					$this->table = $this->morphClass = $this->table.$value; //Pivot relation
+				} else if($this->tableExists($this->table.$value.'_x')){
+					$this->table = $this->morphClass = $this->table.$value.'_x'; //Morph relation
+				}
+			}
+		}
+		parent::__construct(array());
+	}
+
+////////////////////////////////////////////
+
+	//Add these functions to insure that nobody can make them disappear
+	public function delete(){ return false; }
+	public function restore(){ return false; }
+
+	//We do not record history
+	public function setHistory($key=null, $new=null, $old=null, array $parameters = array(), $pivot_type=null, $pivot_id=null){
+		return true;
+	}
+
+	//We do not attach
+	protected function pivots_save(array $parameters = array()){
+		return true;
+	}
+
+////////////////////////////////////////////
+
+}
